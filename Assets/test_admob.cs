7a72428defc36b906a68c94f6f51340cc053b219
Shichao Guan <muqian3288@foxmail.com>
2019-07-25 23:54:51 +0800

    update 测试修改

--- Assets/test_admob.cs
+++ Assets/test_admob.cs
@@ -11,15 +11,14 @@ public class test_admob : MonoBehaviour {
 #if UNITY_ANDROID
             string appId = "ca-app-pub-3940256099942544~3347511713";
 #elif UNITY_IPHONE
-        string appId = "ca-app-pub-3940256099942544~1458002511";
+        string appId = "ca-app-pub-9454451393863828~2171462141";
 #else
             string appId = "unexpected_platform";
 #endif
 
         // Initialize the Google Mobile Ads SDK.
         MobileAds.Initialize(appId);
-
-        this.RequestBanner();
+        Debug.Log("Init AD:"+appId);
     }
 
     private void RequestBanner()
@@ -34,7 +33,23 @@ public class test_admob : MonoBehaviour {
 
         // Create a 320x50 banner at the top of the screen.
         bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+
+        Debug.Log("RequestBannder:"+adUnitId);
     }
 
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("RequestBanner",GUILayout.Width(100),GUILayout.Height(100)))
+        {
+            RequestBanner();
+        }
+    }
 
 }
